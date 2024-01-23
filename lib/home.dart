5bbf6633d375f5fe7698eb5fde404580b66a0efe
Pulkit Raina <pulkitraina2608@gmial.com>
2024-01-23 21:54:23 +0530

    Loading Pages - Update 1

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:namma_metro/bottom_nav_bar.dart';
 
-void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
